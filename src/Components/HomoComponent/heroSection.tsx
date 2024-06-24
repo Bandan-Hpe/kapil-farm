bfcbbd85a556d6d172b9624e0008f5d7d0816d0e
Bandan Pradhan <bandan472000@gmail.com>
2024-06-24 14:31:07 +0530

    changed bg issue

--- src/Components/HomoComponent/heroSection.tsx
+++ src/Components/HomoComponent/heroSection.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import gif from "../../../public/assets/form_bg.gif";
 const HeroSection = () => {
   return (
-    <section className="bg-[url('/assets/LP-image.png')] bg-cover  rounded-3xl lg:flex align-items-center mx-5 bg-center">
+    <section className="relative h-auto  bg-cover bg-center rounded-3xl lg:flex align-items-center mx-5 bg-[url('/assets/LP-image.png')]">
       <div className="lg:w-1/2 lg:px-12 flex items-end sm:justify-center md:justify-center sm:w-full ">
         <h1 className="  text-white font-bold  px-5 lg:text-2xl xl:text-3xl text:5xl uppercase tracking-wider md:text-3xl sm:text-xl py-8">
           Venture into the land <br />
@@ -10,7 +10,7 @@ const HeroSection = () => {
         </h1>
       </div>
       <div className="lg:w-1/2 flex lg:justify-end md:items-end  relative  lg:m-10 sm:justify-center">
-        <div className="bg-lightgolden  rounded-2xl max-w-[500px] sm:my-5">
+        <div className="bg-lightgolden  rounded-2xl max-w-[400px] sm:my-5">
           <div className="px-8 py-8">
             <p className="text-center">
               If you are looking to buy farmland in Hyderabad,
